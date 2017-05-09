77a63107ce3b6dc7ee0148f92e2615b6cca63908
darrelross <darrel.ross@domo.com>
2017-05-09 08:15:06 -0600

    Changed to catfacts api

---
+++ src/main/java/com/darrel/CatFact.java
@@ -0,0 +1,22 @@
+package com.darrel;
+
+public class CatFact {
+    private String[] facts;
+    private boolean success;
+
+    public String[] getFacts() {
+        return facts;
+    }
+
+    public void setFacts(String[] facts) {
+        this.facts = facts;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
--- src/main/java/com/darrel/Texter.java
+++ src/main/java/com/darrel/Texter.java
@@ -1,57 +1,47 @@
 package com.darrel;
 
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.kms.AWSKMS;
-import com.amazonaws.services.kms.AWSKMSClientBuilder;
-import com.amazonaws.services.kms.model.DecryptRequest;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
-import com.amazonaws.services.sns.model.PublishResult;
-import com.amazonaws.util.Base64;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Random;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Texter {
 
-    private final String phoneNumber;
     private final AmazonSNS amazonSNS;
-    private final S3Client s3Client;
 
     public Texter() {
-        this.phoneNumber = decryptKey("PhoneNumber");
-        this.s3Client = new S3Client();
         AmazonSNSClientBuilder builder = AmazonSNSClientBuilder.standard().withCredentials(new DefaultAWSCredentialsProviderChain());
-        builder.setRegion("us-west-2");
+        builder.setRegion(System.getenv("Region"));
         amazonSNS = builder.build();
     }
 
 
     public void sendSMSMessage() {
-        PublishResult result = amazonSNS.publish(new PublishRequest()
+       amazonSNS.publish(new PublishRequest()
                 .withMessage(getCatFact())
-                .withPhoneNumber(phoneNumber));
-        System.out.println(result); // Prints the message ID.
-    }
-
-    private String decryptKey(String key) {
-        byte[] encryptedKey = Base64.decode(System.getenv(key));
-
-        AWSKMS client = AWSKMSClientBuilder.defaultClient();
-
-        DecryptRequest request = new DecryptRequest()
-                .withCiphertextBlob(ByteBuffer.wrap(encryptedKey));
-
-        ByteBuffer plainTextKey = client.decrypt(request).getPlaintext();
-        return new String(plainTextKey.array(), Charset.forName("UTF-8"));
+                .withTopicArn(System.getenv("ARN")));
     }
 
     private String getCatFact() {
-        List<String> catFacts = s3Client.getCatFacts();
-        return catFacts.get(new Random().nextInt(catFacts.size() -1));
+        try {
+            String url = "http://catfacts-api.appspot.com/api/facts?number=1";
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            con.setRequestMethod("GET");
+
+            CatFact catFact = new ObjectMapper().readValue(con.getInputStream(), CatFact.class);
+
+            return catFact.getFacts()[0];
+        } catch (IOException e) {
+            return "Error";
+        }
     }
 
     public static void main() {
