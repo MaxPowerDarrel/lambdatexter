81b11cd3299c87983fde0ab863bff6f5710c06c7
darrelross <darrel.ross@domo.com>
2017-05-09 08:15:06 -0600

    about to change to catfacts api

--- src/main/java/com/darrel/Texter.java
+++ src/main/java/com/darrel/Texter.java
@@ -1,28 +1,20 @@
 package com.darrel;
 
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.kms.AWSKMS;
-import com.amazonaws.services.kms.AWSKMSClientBuilder;
-import com.amazonaws.services.kms.model.DecryptRequest;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
-import com.amazonaws.util.Base64;
 
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Random;
 
 public class Texter {
 
-    private final String phoneNumber;
     private final AmazonSNS amazonSNS;
     private final S3Client s3Client;
 
     public Texter() {
-        this.phoneNumber = decryptKey("PhoneNumber");
         this.s3Client = new S3Client();
         AmazonSNSClientBuilder builder = AmazonSNSClientBuilder.standard().withCredentials(new DefaultAWSCredentialsProviderChain());
         builder.setRegion("us-west-2");
@@ -33,20 +25,7 @@ public class Texter {
     public void sendSMSMessage() {
         PublishResult result = amazonSNS.publish(new PublishRequest()
                 .withMessage(getCatFact())
-                .withPhoneNumber(phoneNumber));
-        System.out.println(result); // Prints the message ID.
-    }
-
-    private String decryptKey(String key) {
-        byte[] encryptedKey = Base64.decode(System.getenv(key));
-
-        AWSKMS client = AWSKMSClientBuilder.defaultClient();
-
-        DecryptRequest request = new DecryptRequest()
-                .withCiphertextBlob(ByteBuffer.wrap(encryptedKey));
-
-        ByteBuffer plainTextKey = client.decrypt(request).getPlaintext();
-        return new String(plainTextKey.array(), Charset.forName("UTF-8"));
+                .withTopicArn("arn:aws:sns:us-west-2:664598357105:CatFacts"));
     }
 
     private String getCatFact() {
