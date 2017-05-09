e32e584b8d5280fa5ae41cff2bac4bff5da439f9
darrelross <darrel.ross@domo.com>
2017-05-09 08:42:56 -0600

    about to change to catfacts api

--- src/main/java/com/darrel/Texter.java
+++ src/main/java/com/darrel/Texter.java
@@ -4,33 +4,44 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
-import com.amazonaws.services.sns.model.PublishResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.List;
-import java.util.Random;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Texter {
 
     private final AmazonSNS amazonSNS;
-    private final S3Client s3Client;
 
     public Texter() {
-        this.s3Client = new S3Client();
         AmazonSNSClientBuilder builder = AmazonSNSClientBuilder.standard().withCredentials(new DefaultAWSCredentialsProviderChain());
-        builder.setRegion("us-west-2");
+        builder.setRegion(System.getenv("Region"));
         amazonSNS = builder.build();
     }
 
 
     public void sendSMSMessage() {
-        PublishResult result = amazonSNS.publish(new PublishRequest()
+       amazonSNS.publish(new PublishRequest()
                 .withMessage(getCatFact())
-                .withTopicArn("arn:aws:sns:us-west-2:664598357105:CatFacts"));
+                .withTopicArn(System.getenv("ARN")));
     }
 
     private String getCatFact() {
-        List<String> catFacts = s3Client.getCatFacts();
-        return catFacts.get(new Random().nextInt(catFacts.size() -1));
+        try {
+            String url = "http://catfacts-api.appspot.com/api/facts?number=1";
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            con.setRequestMethod("GET");
+
+            CatFact catFact = new ObjectMapper().readValue(con.getInputStream(), CatFact.class);
+
+            return catFact.getFacts()[0];
+        } catch (IOException e) {
+            return "Error";
+        }
     }
 
     public static void main() {
